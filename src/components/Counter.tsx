aae57a0493ef92bc4322ab79b9ebd69e8a0c4f03
dhythm <yuta.okada.20@gmail.com>
2022-11-03 23:36:44 +0900

    feat(components): update Counter with memo

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,50 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createMemo, createSignal } from "solid-js";
 
 export const Counter: Component = () => {
   const [count, setCount] = createSignal(0);
-  const doubleCount = () => count() * 2;
+  const fib = createMemo(() => {
+    console.log("Calculation Fibonacci");
+    return fibonacci(count());
+  });
 
-  setInterval(() => setCount(count() + 1), 1000);
-
-  return <div>Count: {doubleCount()}</div>;
+  return (
+    <>
+      <button onClick={() => setCount(count() + 1)}>Count: {count()}</button>
+      <div>
+        1. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        2. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        3. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        4. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        5. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        6. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        7. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        8. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        9. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+      <div>
+        10. {fib()} {fib()} {fib()} {fib()} {fib()}
+      </div>
+    </>
+  );
 };
+
+function fibonacci(num: number): number {
+  if (num <= 1) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
