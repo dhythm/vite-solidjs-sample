0ef22c45b06f7f81ce1ba129bfbb9f91639aaaca
dhythm <yuta.okada.20@gmail.com>
2022-11-03 23:13:33 +0900

    feat(components): update Counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,11 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
 
 export const Counter: Component = () => {
   const [count, setCount] = createSignal(0);
 
-  setInterval(() => setCount(count() + 1), 1000);
+  createEffect(() => {
+    console.log("The count is now", count());
+  });
 
-  return <div>Count: {count()}</div>;
+  return <button onClick={() => setCount(count() + 1)}>Click Me</button>;
 };
