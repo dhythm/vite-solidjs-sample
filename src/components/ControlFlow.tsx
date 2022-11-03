c87dbbac64c622cd114aebd55e1e71a06fafff08
dhythm <yuta.okada.20@gmail.com>
2022-11-03 23:41:59 +0900

    feat(components): implement Control Flow feature in ControlFlow component

---
+++ src/components/ControlFlow.tsx
@@ -0,0 +1,12 @@
+import { Component, createSignal, Show } from "solid-js";
+
+export const ControlFlow: Component = () => {
+  const [loggedIn, setLoggedIn] = createSignal(false);
+  const toggle = () => setLoggedIn(!loggedIn());
+
+  return (
+    <Show when={loggedIn()} fallback={<button onClick={toggle}>Log in</button>}>
+      <button onClick={toggle}>Log out</button>
+    </Show>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { Counter } from "./components/Counter";
+import { ControlFlow } from "./components/ControlFlow";
 
 import "./index.css";
 
-render(() => <Counter />, document.getElementById("root") as HTMLElement);
+render(() => <ControlFlow />, document.getElementById("root") as HTMLElement);
